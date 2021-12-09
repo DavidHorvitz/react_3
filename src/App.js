912c9ca871841f4d0f3396f1b25b6473e9ed6677
DavidHorvitz <2205dzh@gmail.com>
2021-12-09 20:58:34 +0200

    4

--- src/App.js
+++ src/App.js
@@ -1,27 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
 import Greeting from './Greeting';
+import myVar from './components/Vars';
+import * as myImportsObjects from './components/ExportSomeVars';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <Greeting/>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  console.log(myVar());
+  console.log(myImportsObjects.bar);
+  const myChildren = ['Omri ', 'Kfir ', 'Daniel ', 'Michal '];
+  const name = `david`;
+  const yourChildren = [<li>david</li>, <li>rivi</li>, <li>mali</li>, <li>Michal</li>]
+  return <div className="App">
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      {name}
+      <p>Edit<code>src/App.js</code> and save to reload. </p>
+      <Greeting />
+      <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer" >
+        Learn React
+      </a>
+      <p>{myChildren.map(child => <span>{child}</span>)}<ul>asdf{yourChildren}</ul></p>
 
+    </header>
+
+  </div>;
+
+
+};
 export default App;
---
+++ src/components/ExportSomeVars.js
@@ -0,0 +1,4 @@
+export const foo = 'Omri';
+export const bar = 'Kfir';
+export const baz = 'Daniel';
+export const daz = 'Michal';
